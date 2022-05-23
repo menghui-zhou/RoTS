9233d5616b98ab82e968eba358e515c48d8af038
menghui-zhou <102856485+menghui-zhou@users.noreply.github.com>
2022-05-23 21:59:29 +0800

    Update FRoTS.m

--- FRoTS.m
+++ FRoTS.m
@@ -12,20 +12,19 @@ task_num  = length (X);
 dimension = size(X{1}, 1);
 funcVal = [];
 
-% Relation
+
 H = zeros(task_num, task_num - 1);
 H(1 : (task_num + 1) : end) = 1;
 H(2 : (task_num + 1) : end) = -1;
 F = H';
 
-% initial P Q
-% P0 = rand(dimension, task_num) * 100;
-% Q0 = rand(dimension, task_num) * 100;
+
 P0 = zeros(dimension, task_num);
 Q0 = zeros(dimension, task_num);
 
 
-bFlag=0; % this flag tests whether the gradient step only changes a little
+bFlag=0; 
+
 
 Pz= P0;
 Pz_old = P0;
@@ -43,17 +42,16 @@ while iter < opts.maxIter
     
     alpha = (t_old - 1) / t;
       
-    % linear combination
+
     Ps = (1 + alpha) * Pz - alpha * Pz_old;
     Qs = (1 + alpha) * Qz - alpha * Qz_old;
     
-    % compute function value and gradients of the search point
-    % gWs = gPs = gQs
+
     gWs = gradVal_eval(Ps, Qs);
-    Fs = funcVal_eval(Ps, Qs);  % differential part function value
+    Fs = funcVal_eval(Ps, Qs);  
     
     while true
-        % proximal operator of P and Q
+
         Pzp = Proximal_Temporal(Ps - gWs / gamma, rho1 / gamma);
         delta_Pzp = Pzp - Ps;
         Qzp = Proximal_Outlier(Qs - gWs / gamma, rho2 / gamma);
@@ -65,7 +63,7 @@ while iter < opts.maxIter
             sum(sum(delta_Qzp .* gWs)) + gamma / 2 * norm(delta_Qzp, 'fro')^2;
         
         
-        % there is basically no influence of penalties.
+
         if ( norm(Pzp - Ps, 'fro')^2 + norm(Qzp - Qs, 'fro')^2  <= 1e-20)  
             bFlag = 1;
             break;
@@ -87,7 +85,7 @@ while iter < opts.maxIter
     funcVal = cat(1, funcVal, Fzp + nonsmooth_eval(Pzp, Qzp, rho1, rho2));
     
     
-%     test stop condition.
+
     if (bFlag)
         % fprintf('\n The program terminates as the gradient step changes the solution very small.\n');
         break;
@@ -126,7 +124,7 @@ end
 P = Pzp;
 Q = Qzp;
 W = P + Q;
-% private functions
+
 
     function [P_new] = Proximal_Temporal(P, lambda_1)
         
@@ -134,7 +132,7 @@ W = P + Q;
         
         for i = 1 : size(P, 1)
             v = P(i, :); %
-            p0 = zeros(length(v) -1, 1);   % dual variable, starting point
+            p0 = zeros(length(v) -1, 1);  
             p = flsa(v, p0, 0, lambda_1, length(v), 1000, 1e-9, 1, 6);
             P_new(i, :) = p';
         end
@@ -157,7 +155,7 @@ W = P + Q;
         
     end
 
-% smooth part gradient.
+
     function [grad_W] = gradVal_eval(P, Q)
         W = P + Q;
         if opts.pFlag
@@ -174,7 +172,7 @@ W = P + Q;
     end
 
 
-% smooth part func;
+
     function [funcVal] = funcVal_eval(P, Q)
         W = P + Q;
         funcVal = 0;
